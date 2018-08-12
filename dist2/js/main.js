35b62a43d2a9800f741f1d0ae363a3c6180b6b9f
wenzhi <huwenzhi@niuguwang.com>
2018-08-12 16:19:20 +0800

    update dist2

--- dist2/js/main.js
+++ dist2/js/main.js
@@ -163,16 +163,16 @@
           // type对应上面的color与font  若没有对应的 则默认为color1或font1
           dialogue: [
             { type: 6, name: '稳稳', txt: '快过年了，我们去放烟花吧！' },
-            { type: 2, name: '阳阳', txt: '天还这么亮，现在放烟花也不好看。' },
-            { type: 2, name: '阳阳', txt: '再说你那有烟花吗？' },
+            { type: 2, name: '冰冰', txt: '天还这么亮，现在放烟花也不好看。' },
+            { type: 2, name: '冰冰', txt: '再说你那有烟花吗？' },
             { type: 6, name: '稳稳', txt: '我当然有烟花啦。' },
             { type: 6, name: '稳稳', txt: '你那有打火机吗？' },
-            { type: 2, name: '阳阳', txt: '没有呀～' },
+            { type: 2, name: '冰冰', txt: '没有呀～' },
             { type: 6, name: '稳稳', txt: '那你是怎么点燃我的心的？' },
-            { type: 2, name: '阳阳', txt: '。。。' },
-            { type: 2, name: '阳阳', txt: '可是我的心还没有被你点燃呀。' },
+            { type: 2, name: '冰冰', txt: '。。。' },
+            { type: 2, name: '冰冰', txt: '可是我的心还没有被你点燃呀。' },
             { type: 6, name: '稳稳', txt: '别慌，我去买个打火机先。' },
-            { type: 2, name: '阳阳', txt: '打火机可点不燃我的心。' },
+            { type: 2, name: '冰冰', txt: '打火机可点不燃我的心。' },
             { type: 6, name: '稳稳', txt: '天快黑了，我要为你点燃整片天空。' }
           ],
           // 阶段二
